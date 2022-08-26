dd1f3c51cf2c162b8867f1fd63ed0372c36b5195
jsalcedo-dev <61206643+jsalcedo-dev@users.noreply.github.com>
2022-08-26 13:53:59 -0700

    1

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
+
 const app = express();
+
+mongoose.connect("mongodb+srv://jsalcedo6:MorningStar1@cluster0.shrlvxd.mongodb.net/node-angular?retryWrites=true&w=majority")
+.then(() => {
+  console.log('Connected to database!');
+})
+.catch(() =>
+{
+  console.log('Connection failed!');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -14,30 +27,26 @@ app.use((req, res, next) => {
 
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: 'fadf12411l',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id: 'asdfadsfa2134asdf',
-      title: 'Second server-side post',
-      content: 'This is coming from the server'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+  Post.find()
+  .then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched successfully!',
+      posts: documents
+    });
   });
+
 });
 
 module.exports = app;
