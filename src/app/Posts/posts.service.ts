5fee09a5d4ec480188ae5f9d64f74071e86037e9
jsalcedo-dev <61206643+jsalcedo-dev@users.noreply.github.com>
2022-09-12 11:25:53 -0700

    Finished routing section for website development

--- src/app/Posts/posts.service.ts
+++ src/app/Posts/posts.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 import { Post } from './post.model'
+import { response } from 'express';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -33,6 +34,11 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string)
+  {
+    return this.http.get<{_id: string, title: string, content: string}>("http://localhost:3000/api/posts" + id);
+  }
+
   addPost(title: string, content: string){
     const post: Post = {id: null, title: title, content: content};
     this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
@@ -54,4 +60,18 @@ export class PostsService {
       this.postsUpdated.next([...this.posts]);
     });
   }
+
+  updatePost(id: string, title: string, content: string)
+  {
+    const post: Post = { id: id, title: title, content: content};
+    this.http.put("http://localhost:3000/api/posts/" + id, post)
+    .subscribe(response=>{
+      const updatedPosts = [...this.posts];
+      const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+      updatedPosts[oldPostIndex] = post;
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
+    });
+  }
+
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortalTableComponent } from './portal-table/portal-table.component';
+import { PostCreateComponent } from './Posts/post-create/post-create.component';
+import { PostListComponent } from './Posts/post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostListComponent},
+  { path: 'create', component: PostCreateComponent},
+  { path: 'edit/:postId', component: PostCreateComponent},
+  { path: 'fieldPortal', component: PortalTableComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
