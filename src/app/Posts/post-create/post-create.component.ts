7801ae749afc385fd9d8d52dc095ab8ac22b7c08
jsalcedo-dev <61206643+jsalcedo-dev@users.noreply.github.com>
2022-09-19 12:53:55 -0700

    Halfway Image Upload

--- src/app/Posts/post-create/post-create.component.ts
+++ src/app/Posts/post-create/post-create.component.ts
@@ -17,6 +17,7 @@ export class PostCreateComponent implements OnInit {
   enteredContent = "";
   isLoading = false;
   form: FormGroup;
+  imagePreview: string;
   private mode = 'create';
   private postId: string;
   post: Post;
@@ -26,12 +27,10 @@ export class PostCreateComponent implements OnInit {
   ngOnInit(): void {
     this.form = new FormGroup(
       {
-        'title': new FormControl(null, {
-          validators: [Validators.required, Validators.minLength(3)]
+        'title': new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]
         }),
-        'content': new FormControl(null, {
-          validators: [Validators.required]
-        })
+        'content': new FormControl(null, {validators: [Validators.required]}),
+        'image': new FormControl(null, {validators: [Validators.required]})
       });
 
       this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -51,6 +50,17 @@ export class PostCreateComponent implements OnInit {
        }
 
     });
+  }
+
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
